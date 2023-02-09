f68ebc2415e1731d598c111682add13a8e6ee44e
leejunmoo <dlquddnr122@naver.com>
2023-02-09 16:33:24 +0900

    링크 주소 첨부

--- src/components/Project.js
+++ src/components/Project.js
@@ -16,56 +16,56 @@ export default function Project(props) {
         <div className={projectStyle.projectList}>
             {/* 수원여대 */}
             <div className={projectStyle.sww}>
-                <a href="#">
+                <a href="https://leejunmoo.github.io/leejunmoo.swwUniversity/" target='blank'>
                     <img src={process.env.PUBLIC_URL + '/images' + '/project' + '/수원여대실음과.png'}  alt="project1"/>
                 </a>
-                <p className={projectStyle.webP}>&#035;팀프로젝트 &#035;반응형 &#035&#035;CSS</p>
+                <p className={projectStyle.webP}>&#035;팀프로젝트 &#035;반응형 &#035;CSS</p>
                 <div className={projectStyle.btnbox}>
-                    <button><a href="#">GitHub</a></button>
-                    <button><a href="#">Figma</a></button>
+                    <button><a href="https://github.com/leejunmoo/leejunmoo.swwUniversity" target='blank'>GitHub</a></button>
+                    <button><a href="https://www.figma.com/file/7EFSEzO8AmWHjIdvIuELcY/%EC%88%98%EC%9B%90%EC%97%AC%EB%8C%80-%EC%8B%A4%EC%9D%8C%EA%B3%BC-%EB%A6%AC%EB%94%94%EC%9E%90%EC%9D%B8-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8---%EC%8B%9C%EC%95%88?node-id=0%3A1&t=LCwFn69vvoKeaszq-1" target='blank'>Figma</a></button>
                 </div>
             </div>
             {/* 길마로 */}
             <div className="gilmaro">
-                <a href="#">
+                <a href="https://port-0-node-ejs-gilmaro-team-fyyf25lboho2ya.gksl2.cloudtype.app/" target='blank'>
                     <img src={process.env.PUBLIC_URL + '/images' + '/project' + '/길마로.png'}  alt="project1" className={projectStyle.gilmaroImg}/>                    
                 </a>
                 <p>&#035;모바일웹앱 &#035;SQlite &#035;Cookie</p>
                 <div className={projectStyle.btnbox}>
-                    <button><a href="#">GitHub</a></button>
-                    <button><a href="#">Figma</a></button>
+                    <button><a href="https://github.com/leejunmoo/node-ejs-gilmaro-team" target='blank'>GitHub</a></button>
+                    <button><a href="https://www.figma.com/file/R6QVlnWuo8IIaax6AdZIKF/%EB%B8%8C%EB%9E%9C%EB%93%9C%EC%83%B5-%EA%B8%B8%EB%A7%88%EB%A1%9C-%EC%8B%9C%EC%95%88?node-id=0%3A1&t=F4Ml5NSeYXDzbCVV-1" target='blank'>Figma</a></button>
                 </div>
             </div>
             {/* 날씨앱 */}
             <div className="weather-app">
-                <a href="#">
+                <a href="https://leejunmoo.github.io/weather-app/" target='blank'>
                     <img src={process.env.PUBLIC_URL + '/images' + '/project' + '/weatherapp.png'}  alt="project1"/>
                 </a>
                 <p className={projectStyle.webP}>&#035;API &#035;JQuery &#035;JavaScript</p>
                 <div className={projectStyle.btnbox}>
-                    <button><a href="#">GitHub</a></button>
+                    <button><a href="https://github.com/leejunmoo/weather-app" target='blank'>GitHub</a></button>
                 </div>
             </div>
             {/* 문학수첩 */}
             <div className="library-note">
-                <a href="#">
+                <a href="https://leejunmoo.github.io/literary-notebook/" target='blank'>
                     <img src={process.env.PUBLIC_URL + '/images' + '/project' + '/문학수첩.png'}  alt="project1"/>
                 </a>
                 <p className={projectStyle.webP}>&#035;반응형 &#035;JQuery &#035;포토샵디자인</p>
                 <div className={projectStyle.btnbox}>
-                    <button><a href="#">GitHub</a></button>
-                    <button><a href="#">Figma</a></button>
+                    <button><a href="https://github.com/leejunmoo/literary-notebook" target='blank'>GitHub</a></button>
+                    <button><a href="https://www.figma.com/file/wYFN10PHBga9ddQ0ju3gyP/%EB%AC%B8%ED%95%99%EC%88%98%EC%B2%A9-%EC%8B%9C%EC%95%88_22-12-05?node-id=0%3A1&t=cdHbffSB5fMcNrAs-1" target='blank'>Figma</a></button>
                 </div>
             </div>
             {/* 연하장 */}
             <div className="newyear">
-                <a href="#">
+                <a href="https://port-0-node-ejs-post-newyears-fyyf25lboho2ya.gksl2.cloudtype.app/" target='blank'>
                     <img src={process.env.PUBLIC_URL + '/images' + '/project' + '/연하장.png'}  alt="project1" className={projectStyle.gilmaroImg}/>
                 </a>
                 <p>&#035;모바일웹앱 &#035;json &#035;node.js</p>
                 <div className={projectStyle.btnbox}>
-                    <button><a href="#">GitHub</a></button>
-                    <button><a href="#">Figma</a></button>
+                    <button><a href="https://github.com/leejunmoo/node-ejs-post-newyears" target='blank'>GitHub</a></button>
+                    <button><a href="https://www.figma.com/file/Hw7GUPwAsUj4oNFmdo77Hj/%EC%97%B0%ED%95%98%EC%9E%A5%EC%A0%9C%EC%9E%91?node-id=0%3A1&t=PbOagcEf4l0optJ0-1" target='blank'>Figma</a></button>
                 </div>
             </div>
         </div>
