9827e4a16d97c7989a33baf9bb7d5c141be7e7a5
leejunmoo <dlquddnr122@naver.com>
2023-02-14 17:48:59 +0900

    hover 이벤트 등록

--- src/components/Project.js
+++ src/components/Project.js
@@ -14,6 +14,7 @@ export default function Project(props) {
 
     const handleMouseOver = () => {
         setHovering(true);
+        console.log(hovering)
       };
 
       const handleMouseOut = () => {
@@ -33,7 +34,10 @@ export default function Project(props) {
                  onMouseOut={handleMouseOut}
                 >
                     <img src={process.env.PUBLIC_URL + '/images' + '/project' + '/수원여대실음과.png'}  alt="수원여자대학교실용음과프로젝트" className={projectStyle.hoverbox} />
-                    <div className={hovering ? `${projectStyle.hovertext}` : `${projectStyle.nohover}`} ><p>수원여자대학교</p></div>
+                    <div 
+                    className={hovering ? `${projectStyle.hovertext}` : `${projectStyle.nohover}`}>
+                        <p>수원여자대학교</p>
+                    </div>
                 </a>
                 <p className={projectStyle.webP}>&#035;팀프로젝트 &#035;반응형 &#035;CSS</p>
                 <div className={projectStyle.btnbox}>
@@ -43,8 +47,15 @@ export default function Project(props) {
             </div>
             {/* 길마로 */}
             <div className="gilmaro">
-                <a href="https://port-0-node-ejs-gilmaro-team-fyyf25lboho2ya.gksl2.cloudtype.app/" target='blank' className={projectStyle.re}>
-                    <img src={process.env.PUBLIC_URL + '/images' + '/project' + '/길마로.png'}  alt="브랜드샵길마로" className={projectStyle.gilmaroImg}/>                    
+                <a href="https://port-0-node-ejs-gilmaro-team-fyyf25lboho2ya.gksl2.cloudtype.app/" target='blank' className={projectStyle.re}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+                >
+                    <img src={process.env.PUBLIC_URL + '/images' + '/project' + '/길마로.png'}  alt="브랜드샵길마로" className={projectStyle.gilmaroImg}/>
+                    <div 
+                    className={hovering ? `${projectStyle.hovertext2}` : `${projectStyle.nohover}`}>
+                        <p>브랜드샵 길마로</p>
+                    </div>                    
                 </a>
                 <p>&#035;모바일웹앱 &#035;SQlite &#035;Cookie</p>
                 <div className={projectStyle.btnbox}>
@@ -54,8 +65,15 @@ export default function Project(props) {
             </div>
             {/* 날씨앱 */}
             <div className="weather-app">
-                <a href="https://leejunmoo.github.io/weather-app/" target='blank' className={projectStyle.re}>
+                <a href="https://leejunmoo.github.io/weather-app/" target='blank' className={projectStyle.re}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+                >
                     <img src={process.env.PUBLIC_URL + '/images' + '/project' + '/weatherapp.png'}  alt="날씨앱" className={projectStyle.hoverbox}/>
+                    <div 
+                    className={hovering ? `${projectStyle.hovertext}` : `${projectStyle.nohover}`}>
+                        <p>날씨앱</p>
+                    </div>
                 </a>
                 <p className={projectStyle.webP}>&#035;API &#035;JQuery &#035;JavaScript</p>
                 <div className={projectStyle.btnbox}>
@@ -64,8 +82,15 @@ export default function Project(props) {
             </div>
             {/* 문학수첩 */}
             <div className="library-note">
-                <a href="https://leejunmoo.github.io/literary-notebook/" target='blank' className={projectStyle.re}>
+                <a href="https://leejunmoo.github.io/literary-notebook/" target='blank' className={projectStyle.re}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+                >
                     <img src={process.env.PUBLIC_URL + '/images' + '/project' + '/문학수첩.png'}  alt="문학수첩" className={projectStyle.hoverbox}/>
+                    <div 
+                    className={hovering ? `${projectStyle.hovertext}` : `${projectStyle.nohover}`}>
+                        <p>문학수첩</p>
+                    </div>
                 </a>
                 <p className={projectStyle.webP}>&#035;반응형 &#035;JQuery &#035;포토샵디자인</p>
                 <div className={projectStyle.btnbox}>
@@ -75,9 +100,17 @@ export default function Project(props) {
             </div>
             {/* 연하장 */}
             <div className="newyear">
-                <a href="https://port-0-node-ejs-post-newyears-fyyf25lboho2ya.gksl2.cloudtype.app/" target='blank' className={projectStyle.re}>
+                <a href="https://port-0-node-ejs-post-newyears-fyyf25lboho2ya.gksl2.cloudtype.app/" target='blank' className={projectStyle.re}
+                onMouseOver={handleMouseOver}
+                onMouseOut={handleMouseOut}
+                >
                     <img src={process.env.PUBLIC_URL + '/images' + '/project' + '/연하장.png'}  alt="연하장" className={projectStyle.gilmaroImg}/>
+                    <div 
+                    className={hovering ? `${projectStyle.hovertext2}` : `${projectStyle.nohover}`}>
+                        <p>모바일 연하장</p>
+                    </div>
                 </a>
+                
                 <p>&#035;모바일웹앱 &#035;json &#035;node.js</p>
                 <div className={projectStyle.btnbox}>
                     <button><a href="https://github.com/leejunmoo/node-ejs-post-newyears" target='blank'>GitHub</a></button>
